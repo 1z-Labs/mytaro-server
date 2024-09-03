f1fc54f343d579aadfc30b5075ea2f47cacdb0f0
Day <s2208@e-mirim.hs.kr>
2024-09-03 11:28:48 +0900

    modify. migrate 할 database 체인지

--- db/schema.rb
+++ db/schema.rb
@@ -11,4 +11,63 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 0) do
+  create_table "Category", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon", null: false
+  end
+
+  create_table "Chapter", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "index", null: false
+    t.string "content", null: false
+    t.string "contentId"
+    t.index ["contentId"], name: "contentId"
+  end
+
+  create_table "Content", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.string "imagePath", null: false
+    t.string "content", null: false
+    t.integer "clover", null: false
+  end
+
+  create_table "ContentCategory", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "contentId"
+    t.string "categoryId"
+  end
+
+  create_table "InputInfo", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.column "gender", "enum('male','female')", null: false
+    t.string "birthday", null: false
+    t.string "birthClock"
+    t.string "pName", null: false
+    t.column "pGender", "enum('male','female')", null: false
+    t.string "pBirthday", null: false
+    t.string "pBirthClock"
+    t.string "userId"
+    t.index ["userId"], name: "userId"
+  end
+
+  create_table "Preview", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "imagePath", null: false
+    t.string "contentId"
+  end
+
+  create_table "Review", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "review", null: false
+    t.string "author", null: false
+    t.timestamp "created_at"
+    t.string "contentId"
+    t.string "authorId"
+  end
+
+  create_table "User", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "phone", null: false
+    t.boolean "isBeforePurchased", default: false, null: false
+  end
+
+  add_foreign_key "Chapter", "Content", column: "contentId", name: "Chapter_ibfk_1"
+  add_foreign_key "InputInfo", "User", column: "userId", name: "InputInfo_ibfk_1"
 end
