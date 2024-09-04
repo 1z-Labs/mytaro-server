42eb3d96eb665dc5a38a1e9e3b01d779289e0360
fire-irror <firenoah.m@gmail.com>
2024-09-04 22:25:37 +0900

    fix : 변수이름 수정, 계산 수정

--- app/helpers/saju_helper.rb
+++ app/helpers/saju_helper.rb
@@ -1,41 +1,42 @@
 # app/helpers/saju_helper.rb
 module SajuHelper
-  TEN_GAN = ['갑', '을', '병', '정', '무', '기', '경', '신', '임', '계']
-  EARTHLY_BRANCHES = ['자', '축', '인', '묘', '진', '사', '오', '미', '신', '유', '술', '해']
+  GAN = ['갑', '을', '병', '정', '무', '기', '경', '신', '임', '계']
+  JI = ['자', '축', '인', '묘', '진', '사', '오', '미', '신', '유', '술', '해']
 
   def saju_for(lunar_date, birthtime)
     {
-      year_pillar: calculate_year_pillar(lunar_date.year),
-      month_pillar: calculate_month_pillar(lunar_date.month),
-      day_pillar: calculate_day_pillar(lunar_date.day),
-      time_pillar: calculate_time_pillar(birthtime)
+      year: calculate_year(lunar_date.year),
+      month: calculate_month(lunar_date.month),
+      day: calculate_day(lunar_date.day),
+      time: calculate_time(birthtime)
     }
   end
 
   private
 
-  def calculate_year_pillar(year)
+  def calculate_year(year)
     gan_index = (year - 4) % 10
-    branch_index = (year - 4) % 12
-    "#{TEN_GAN[gan_index]}#{EARTHLY_BRANCHES[branch_index]}"
+    ji_index = (year - 4) % 12
+    "#{GAN[gan_index]}#{JI[ji_index]}"
   end
 
-  def calculate_month_pillar(month)
-    gan_index = (month + 1) % 10
-    branch_index = (month + 1) % 12
-    "#{TEN_GAN[gan_index]}#{EARTHLY_BRANCHES[branch_index]}"
+  def calculate_month(month)
+    gan_index = (month + 3) % 10
+    ji_index = (month + 1) % 12
+    "#{GAN[gan_index]}#{JI[ji_index]}"
+
   end
 
-  def calculate_day_pillar(day)
-    gan_index = (day + 5) % 10
-    branch_index = (day + 5) % 12
-    "#{TEN_GAN[gan_index]}#{EARTHLY_BRANCHES[branch_index]}"
+  def calculate_day(day)
+    gan_index = (day + 7) % 10
+    ji_index = (day - 3) % 12
+    "#{GAN[gan_index]}#{JI[ji_index]}"
   end
 
-  def calculate_time_pillar(birthtime)
+  def calculate_time(birthtime)
     hour = birthtime.hour
-    branch_index = (hour / 2) % 12
-    gan_index = (hour + 1) % 10
-    "#{TEN_GAN[gan_index]}#{EARTHLY_BRANCHES[branch_index]}"
+    ji_index = (hour / 2) % 12
+    gan_index = (hour / 2) % 10
+    "#{GAN[gan_index]}#{JI[ji_index]}"
   end
 end
