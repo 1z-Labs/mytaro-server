3bf27576bc8b17e743ec4247172fb9348271b94c
Day <s2208@e-mirim.hs.kr>
2024-09-04 14:45:32 +0900

    feat. 카테고리별 컨텐츠를 리뷰순으로 내림차순 정보 API 생성

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   # /contents/popular
   get '/contents/popular', to: 'contents#popular'
 
+  # 카테고리 별 각 컨텐츠의 리뷰 순으로 나열하는 API
+  get 'contents/category/:category_id/reviews', to: 'contents#index_by_reviews'
+
   resources :my_infos do
     member do
       # GET /my_infos/:id/p_fields
