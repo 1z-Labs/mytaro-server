63c21ec6e9e0a4e9f2ec7d9511b76a28446e416e
Day <s2208@e-mirim.hs.kr>
2024-09-10 12:39:19 +0900

    modify. 카테고리 uuid > id (auto_increment) 변환

--- db/migrate/20240903052535_create_categories.rb
+++ db/migrate/20240903052535_create_categories.rb
@@ -1,10 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
-    create_table :categories do |t|
+    create_table :categories, id: :integer, primary_key: :id, auto_increment: true do |t|
       t.string :name
       t.string :icon
-
-      t.timestamps
     end
   end
 end
